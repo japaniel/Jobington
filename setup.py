fe66651db6fc3eec0e81b9d6cd197e84aca55503
Yartard <yartard@cathub.io>
2017-05-11 09:26:32 -0600

    Initial commit with some (likely not working) place holder files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+      name='Jobington',
+      version='1.0',
+      description='Jobs in Jenkins need both learning and Jobbing',
+      long_description='Really guys I just need sometihng to test building
+                        things in Jenkins'
+      author='Daniel Redington',
+      author_email='japaniel@japaniel.com',
+      packages=['jobington'],
+     )
